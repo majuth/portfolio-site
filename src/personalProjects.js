ea5640a2104ec396956731e2fca4ee3951749247
Majuth <majuth101@hotmail.com>
2021-02-22 18:14:46 -0500

    Updated personal projects

--- src/personalProjects.js
+++ src/personalProjects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import portfolioSite from './portfolioSite.png';
 import basketballStatsSite from './basketballStatsSite.png';
+import imageRepository from './imageRepository.png';
 
 class PersonalProjects extends React.Component {
     render() {
@@ -37,12 +38,36 @@ class PersonalProjects extends React.Component {
                 <div class="card-header" id="headingOne">
                   <h5 class="mb-0">
                     <button class="btn btn-link collapsed" style={{color: "white", fontWeight: "600"}} data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                      Personal Portfolio Webpage
+                      Image Repository
                     </button>
                   </h5>
                 </div>
 
                 <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                  <div class="card-body">
+                    <span style={{fontWeight: "600"}}> Uses:</span> React and Firebase
+                    <br></br>
+                    <span style={{fontWeight: "600"}}>Description:</span> An image repository that can have images added and stored on a server
+                    <br></br>
+                    <br></br>
+                    <img src={imageRepository} class="mx-auto d-block" style={{width: "100%"}}></img>
+                    <br></br>
+                    <div style={{backgroundColor: "#343a40", width: "156px", margin: "auto"}}>
+                    <a class="githubLink" href="https://github.com/majuthkira/image-repository"><div class="githubButton">View on Github <img src="https://image.flaticon.com/icons/svg/25/25231.svg" alt="github" style={{width:"19px", paddingLeft: "4px"}}></img></div></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card contentCard projectContainer" style={{borderRadius: "0", backgroundColor: "#66757f", maxWidth: "80vw", padding: "0"}}>
+                <div class="card-header" id="headingTwo">
+                  <h5 class="mb-0">
+                    <button class="btn btn-link collapsed" style={{color: "white", fontWeight: "600"}} data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                      Personal Portfolio Webpage
+                    </button>
+                  </h5>
+                </div>
+
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
                   <div class="card-body">
                     <span style={{fontWeight: "600"}}> Uses:</span> React, Bootstrap, HTML, CSS, and Javascript
                     <br></br>
@@ -57,22 +82,8 @@ class PersonalProjects extends React.Component {
                   </div>
                 </div>
               </div>
-              <div class="card contentCard projectContainer" style={{borderRadius: "0", backgroundColor: "#66757f", maxWidth: "80vw", padding: "0"}}>
-                <div class="card-header" id="headingTwo">
-                  <h5 class="mb-0">
-                    <button class="btn btn-link collapsed" style={{color: "white", fontWeight: "600"}} data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                      More personal projects coming
-                    </button>
-                  </h5>
-                </div>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-                  <div class="card-body">
-                    Check back for more projects currently in the works
-                  </div>
-                </div>
-              </div>
-            </div>
             <br></br>
+            </div>
         </div>
       );
     }
